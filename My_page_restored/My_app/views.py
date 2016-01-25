fa127975cde113bd297c43cfeb883318ecdea1bd
cedoor <omardesogus9@gmail.com>
2016-01-25 17:05:11 +0100

    Not found 404 added

--- My_page_restored/My_app/views.py
+++ My_page_restored/My_app/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 def hello_world(request):
-  html = "<html><body><p>Hello, world!</p></body></html>"
-  return HttpResponse(html)
+    html = "<html><body><p>Hello, world!</p></body></html>"
+    return HttpResponse(html)
+
+def not_found(request):
+    html = HttpResponseNotFound('<h1>Pagina non trovata </h1>')
+    return HttpResponse(html)
