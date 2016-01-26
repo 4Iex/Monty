078e95bbfe4b9c88de41263d3481d73ca5faad2e
cedoor <omardesogus9@gmail.com>
2016-01-26 17:26:04 +0100

    header currently finished

--- My_page_restored/urls.py
+++ My_page_restored/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from My_app import views
+from .My_app import views
 
 urlpatterns = [
     url(r'^profile$', views.profile ),
